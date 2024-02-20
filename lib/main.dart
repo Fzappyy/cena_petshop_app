8aa3257475912cf554dd3a21d447c072597688ba
Felix Cena Jr. <felixcena15@gmail.com>
2024-02-21 01:01:34 +0800

    onboarding ui good, no slider yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cena_petshop_app/screens/onboarding.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,18 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: SafeArea(
-          child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Container(
-              height: 100,
-              width: MediaQuery.of(context).size.width * .90,
-              color: Colors.amberAccent),
-        ),
-      )),
-    ));
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: Onboarding());
   }
 }
